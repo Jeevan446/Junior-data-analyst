a659df070a238d4c26fe5a859c70922861ee7a04
Jeevan446 <neupanejeevan446@gmail.com>
2026-07-15 22:26:48 +0545

    basic endpoint created

--- Backend/database/queries.py
+++ Backend/database/queries.py
@@ -50,7 +50,32 @@ def add_users(user_id,company_type=None,company_description=None):
         if conn:
             conn.close()
 
+
+
+
 # searching in user table that the id exists or not
+def search_user(user_id):
+    try:
+        conn=None
+        cursor=None
+        conn=db_connect()
+        cursor=conn.cursor()
+        cursor.execute('''
+        SELECT user_id FROM users WHERE user_id=%s
+        ''',(user_id,))
+        data=cursor.fetchone()
+        return data
+    except Exception as e:
+        print("Error while searching for users",e)
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+
+
+
 
         
     
--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,40 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from routes.users import router as user_router
 load_dotenv()
 app=FastAPI()
+
+app.include_router(user_router)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # from database.queries import user_table
@@ -9,3 +42,14 @@ app=FastAPI()
  
 # from database.queries import add_users
 # add_users("Namea","HEllo","WOrld")
+
+
+
+from database.queries import search_user
+user=search_user('Nameas')
+# print(user)
+
+if user:
+    print('User found')
+else:
+    print("User not found")
