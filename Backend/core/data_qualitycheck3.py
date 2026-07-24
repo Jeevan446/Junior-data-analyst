cb9575a9bab7fad5f7d75d4fc8497f1fd39e4ac5
Jeevan446 <neupanejeevan446@gmail.com>
2026-07-24 07:57:44 +0545

    duplicated columns checking logic added

--- Backend/core/data_qualitycheck3.py
+++ Backend/core/data_qualitycheck3.py
@@ -3,6 +3,7 @@ def check_data_quality(df_arr):
         check_missing_values(df_arr)
     except Exception as e:
         print("Error while cheking data quality")
+        raise
       
 
 def check_missing_values(df_arr):
@@ -14,17 +15,26 @@ def check_missing_values(df_arr):
             }
             df_dict['data_quality']=data_quality
         check_empty_strings(df_arr)
-        print(df_arr)
     except Exception as e:
         print("Error while checking for missing values",e)
+        raise
     
 def check_empty_strings(df_arr):
     try:
         for df_dict in df_arr:
             empty_strings=(df_dict['dataframe'].astype(str).apply(lambda col: col.str.strip() == "").sum().to_dict())
             df_dict['data_quality']['Empty strings']=empty_strings
-        print(df_arr)
+        check_duplicated(df_arr)
     except Exception as e:
         print("Error while checking for missing values",e)
+        raise
 
-    
+
+def check_duplicated(df_arr):
+    try:
+        for df_dict in df_arr:
+            df_dict['data_quality']['duplicate columns']=int(df_dict['dataframe'].duplicated().sum())
+        print(df_arr)
+    except Exception as e:
+        print("Error while analyzing duplicated columns")
+        raise
